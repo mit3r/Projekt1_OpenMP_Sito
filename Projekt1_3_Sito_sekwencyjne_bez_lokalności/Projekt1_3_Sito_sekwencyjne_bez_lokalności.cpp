2f889b3589002dbe8c6c77c43421c735cbf546d1
Kamil <kamil.suwiczak@wp.pl>
2026-05-11 17:59:19 +0200

    fixed sito sekwe bez lokal

--- Projekt1_3_Sito_sekwencyjne_bez_lokalności/Projekt1_3_Sito_sekwencyjne_bez_lokalności.cpp
+++ Projekt1_3_Sito_sekwencyjne_bez_lokalności/Projekt1_3_Sito_sekwencyjne_bez_lokalności.cpp
@@ -1,62 +1,89 @@
 ﻿#include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <omp.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	// Ustalanie zakresu
 	int m = 2, n = pow(2, 20);
 
-	for(int i = 0; i < argc; i++) {
-		if(!strcmp(argv[i], "-m") && i + 1 < argc) 
+	for (int i = 0; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-m") && i + 1 < argc)
 			m = atoi(argv[i + 1]);
-		if(!strcmp(argv[i], "-n") && i + 1 < argc) 
+		if (!strcmp(argv[i], "-n") && i + 1 < argc)
 			n = atoi(argv[i + 1]);
 	}
 
 	// Właściwy algorytm
+	int limit = sqrt(n);
+	bool *primeArray = new bool[limit + 1];
+	std::memset(primeArray, true, (limit + 1) * sizeof(bool));
+	primeArray[0] = primeArray[1] = false;
 
-	bool* result = new bool[n - m + 1];
-	std::memset(result, true, (n - m + 1) * sizeof(bool));
+	for (int i = 2; i * i <= limit; i++)
+	{
+		if (primeArray[i])
+		{
+			for (int j = i * i; j <= limit; j += i)
+			{
+				primeArray[j] = false;
+			}
+		}
+	}
 
-	bool* primeArray = new bool[(int)(sqrt(n) + 1)];
-	std::memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
+	// Sito dla przedziału [m, n]
+	int rangeSize = n - m + 1;
+	bool *result = new bool[rangeSize];
+	std::memset(result, true, rangeSize * sizeof(bool));
 
-	for(int i = 2; i * i * i * i <= n; i++) {
-		if(primeArray[i] == false) 
+	double startTime = omp_get_wtime();
+	for (int i = 2; i <= limit; i++)
+	{
+		if (!primeArray[i])
 			continue;
 
-		for(int j = i * i; j * j <= n; j++) 
-			primeArray[j] = false;
-	}
+		int firstMultiple = (m / i) * i;
+		if (firstMultiple < m)
+			firstMultiple += i;
 
-	for(int i = 2; i * i <= n; i++) {
-		if(!primeArray[i]) continue;
+		if (firstMultiple <= i)
+			firstMultiple = i * 2;
 
-		int firstMultiple = (m / i);
-
-		if(firstMultiple <= 1)
-			firstMultiple = 2 * i;
-		else {
-			firstMultiple = firstMultiple * i;
-			if(m % i) firstMultiple += i;
-		}
-
-		for(int j = firstMultiple; j <= n; j += i) 
+		for (int j = firstMultiple; j <= n; j += i)
+		{
 			result[j - m] = false;
+		}
 	}
+	double endTime = omp_get_wtime();
+	std::cout << "Czas obliczania liczb pierwszych w przedziale [m, n]: " << endTime - startTime << " sekund" << std::endl;
 
 	// Wypisywanie wyników do pliku, jeśli podano flagę -o
 	bool doPrint = false;
-	for(int i = 0; i < argc; i++) doPrint = !strcmp(argv[i], "-o");
+	for (int i = 0; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-o"))
+			doPrint = true;
+	}
 
-	if(doPrint) {
+	if (doPrint)
+	{
 		std::fstream file("primes_3.txt", std::ios::out);
-
-		for(int i = m; i <= n; i++)
-			if(result[i - m]) file << i << std::endl;
-
+		for (int i = m; i <= n; i++)
+		{
+			if (result[i - m])
+				file << i << '\n';
+		}
+		std::cout << "dlugsc listy: " << std::count(result, result + rangeSize, true) << std::endl;
 		file.close();
 	}
+
+	delete[] primeArray;
+	delete[] result;
 
 	return 0;
 }
