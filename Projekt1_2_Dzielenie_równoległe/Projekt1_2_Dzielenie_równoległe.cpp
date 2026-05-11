69d46b0a65e9136645627c59ad1a3774761fdfb4
Kamil <kamil.suwiczak@wp.pl>
2026-05-11 14:45:03 +0200

    edited sito_rownolegle and added num_of_primes

--- Projekt1_2_Dzielenie_równoległe/Projekt1_2_Dzielenie_równoległe.cpp
+++ Projekt1_2_Dzielenie_równoległe/Projekt1_2_Dzielenie_równoległe.cpp
@@ -1,61 +1,111 @@
 ﻿#include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <cmath>
+#include <cstring>
+#include <omp.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	// Ustalanie zakresu
 	int m = 2, n = pow(2, 20);
 
-	for(int i = 0; i < argc; i++) {
-		if(!strcmp(argv[i], "-m") && i + 1 < argc)
+	for (int i = 0; i < argc; i++)
+	{
+		if (!strcmp(argv[i], "-m") && i + 1 < argc)
 			m = atoi(argv[i + 1]);
-		if(!strcmp(argv[i], "-n") && i + 1 < argc)
+		if (!strcmp(argv[i], "-n") && i + 1 < argc)
 			n = atoi(argv[i + 1]);
 	}
 
 	// Właściwy algorytm
 
-	bool* result = new bool[n - m + 1];
+	bool *result = new bool[n - m + 1];
 	std::memset(result, true, (n - m + 1) * sizeof(bool));
 
-	bool* primeArray = new bool[(int)(sqrt(n) + 1)];
+	bool *result2 = new bool[n - m + 1];
+	std::memset(result2, true, (n - m + 1) * sizeof(bool));
+
+	bool *primeArray = new bool[(int)(sqrt(n) + 1)];
 	std::memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
 
-	for (int i = 2; i* i <= n; i++) {
-		for(int j = 2; j * j <= i; j++) {
-			if(primeArray[j] == true && i % j == 0) {
-				primeArray[i] = false; 
-				break; 
+	double startTime = omp_get_wtime();
+	for (int i = 2; i * i <= n; i++)
+	{
+		for (int j = 2; j * j <= i; j++)
+		{
+			if (primeArray[j] == true && i % j == 0)
+			{
+				primeArray[i] = false;
+				break;
+			}
+		}
+	}
+	double endTime = omp_get_wtime();
+	std::cout << "Czas obliczania liczb pierwszych do sqrt(n): " << endTime - startTime << " sekund" << std::endl;
+
+	startTime = omp_get_wtime();
+	for (int i = m; i <= n; i++)
+	{
+		for (int j = 2; j * j <= i; j++)
+		{
+			if (primeArray[j] == true && i % j == 0)
+			{
+				result[i - m] = false;
+				break;
 			}
 		}
 	}
 
-	#pragma omp parallel
+	endTime = omp_get_wtime();
+	std::cout << "Czas obliczania sekwencyjnych liczb pierwszych w przedziale [m, n]: " << endTime - startTime << " sekund" << std::endl;
+
+	startTime = omp_get_wtime();
+
+#pragma omp parallel
 	{
-		// ? Jaki podział pracy ?
-		#pragma omp for 
-		for(int i = m; i <= n; i++) {
-			for(int j = 2; j * j <= i; j++) {
-				if(primeArray[j] == true && i % j == 0) { 
-					result[i - m] = false; 
-					break; 
+// ? Jaki podział pracy ?
+#pragma omp for schedule(dynamic)
+		for (int i = m; i <= n; i++)
+		{
+			for (int j = 2; j * j <= i; j++)
+			{
+				if (primeArray[j] == true && i % j == 0)
+				{
+					result2[i - m] = false;
+					break;
 				}
 			}
 		}
 	}
+	endTime = omp_get_wtime();
+	std::cout << "Czas obliczania liczb pierwszych w przedziale [m, n]: " << endTime - startTime << " sekund" << std::endl;
 
 	// Wypisywanie wyników do pliku, jeśli podano flagę -o
 	bool doPrint = false;
-	for(int i = 0; i < argc; i++) doPrint = !strcmp(argv[i], "-o");
+	for (int i = 0; i < argc; i++)
+		doPrint = !strcmp(argv[i], "-o");
 
-	if(doPrint) {
+	if (doPrint)
+	{
+		// sekwencyjnie
 		std::fstream file("primes_1.txt", std::ios::out);
 
-		for(int i = m; i <= n; i++)
-			if(result[i - m]) file << i << std::endl;
+		for (int i = m; i <= n; i++)
+			if (result[i - m])
+				file << i << std::endl;
 
 		file.close();
+		// równolegle
+		std::fstream file2("primes_2.txt", std::ios::out);
+		for (int i = m; i <= n; i++)
+			if (result2[i - m])
+				file2 << i << std::endl;
+		file2.close();
 	}
 
+	delete[] result;
+	delete[] result2;
+	delete[] primeArray;
 	return 0;
 }
--- tests/test.py
+++ tests/test.py
@@ -4,10 +4,10 @@ n = sys.argv[1] if len(sys.argv) > 1 else "20"
 k = sys.argv[2] if len(sys.argv) > 2 else "50"
 
 with open(f"generated_primes/primes2^{n}.txt", "r") as file:
-    primes = file.read().splitlines() [:int(k)]
+    primes = file.read().splitlines() 
 
-with open("primes_3.txt", "r") as file:
-    primes2 = file.read().splitlines() [:int(k)]
+with open("../Projekt1_2_Dzielenie_równoległe/primes_2.txt", "r") as file:
+    primes2 = file.read().splitlines()
 
 
 print(primes)
@@ -17,3 +17,6 @@ if primes == primes2:
     print("\nThe lists are the same.")
 else:
     print("\nThe lists are different.")
+
+print(f"Number of primes in the first list: {len(primes)}")
+print(f"Number of primes in the second list: {len(primes2)}")
