9e3ad9ed4ac9534c61097bc405e4de83050da60f
Kamil <kamil.suwiczak@wp.pl>
2026-05-12 13:32:15 +0200

    stworzone sito

--- Projekt1_4_Sito_sekwencyjne_z_lokalnoscia/Projekt1_4_Sito_sekwencyjne_z_lokalnoscia.cpp
+++ Projekt1_4_Sito_sekwencyjne_z_lokalnoscia/Projekt1_4_Sito_sekwencyjne_z_lokalnoscia.cpp
@@ -1,19 +1,129 @@
 ﻿// sito sekwencyjne z lokalnością "domenowe" [k3a]
-
 #include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+#include <omp.h>
 
-int main()
+//64 KB pamięci
+constexpr int SEGMENT_SIZE = 65536;
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello World!\n";
+    // Ustalanie zakresu
+    int m = 2;
+    int n = pow(2, 20);
+
+    for (int i = 0; i < argc; i++)
+    {
+        if (!strcmp(argv[i], "-m") && i + 1 < argc)
+            m = std::atoi(argv[i + 1]);
+        if (!strcmp(argv[i], "-n") && i + 1 < argc)
+            n = pow(2, std::atoi(argv[i + 1]));
+    }
+
+    double startTime = omp_get_wtime();
+    int limit = std::sqrt(n);
+    bool *basePrimes = new bool[limit + 1];
+    std::memset(basePrimes, true, (limit + 1) * sizeof(bool));
+    basePrimes[0] = basePrimes[1] = false;
+
+    for (int i = 2; i * i <= limit; i++)
+    {
+        if (basePrimes[i])
+        {
+            for (int j = i * i; j <= limit; j += i)
+            {
+                basePrimes[j] = false;
+            }
+        }
+    }
+
+    int *primes = new int[limit];
+    int primeCount = 0;
+    for (int i = 2; i <= limit; i++)
+    {
+        if (basePrimes[i])
+        {
+            primes[primeCount++] = i;
+        }
+    }
+
+    bool *segment = new bool[SEGMENT_SIZE];
+
+    int totalPrimeCount = 0;
+    bool doPrint = false;
+    for (int i = 0; i < argc; i++)
+    {
+        if (!strcmp(argv[i], "-o"))
+        {
+            doPrint = true;
+            break;
+        }
+    }
+
+    std::fstream file;
+    if (doPrint)
+        file.open("primes.txt", std::ios::out);
+
+    for (int low = m; low <= n; low += SEGMENT_SIZE)
+    {
+        int high = std::min(low + SEGMENT_SIZE - 1, n);
+        int currentSegmentSize = high - low + 1;
+
+        std::memset(segment, true, currentSegmentSize * sizeof(bool));
+
+        for (int p = 0; p < primeCount; p++)
+        {
+            int i = primes[p];
+
+            int firstMultiple = (low / i);
+            if (firstMultiple <= 1)
+            {
+                firstMultiple = i + i;
+            }
+            else if (low % i != 0)
+            {
+                firstMultiple = (firstMultiple * i) + i;
+            }
+            else
+            {
+                firstMultiple = (firstMultiple * i);
+            }
+
+            for (int j = firstMultiple; j <= high; j += i)
+            {
+                segment[j - low] = false;
+            }
+        }
+
+        for (int i = 0; i < currentSegmentSize; i++)
+        {
+            if (segment[i])
+            {
+                totalPrimeCount++;
+                if (doPrint)
+                    file << (low + i) << "\n";
+            }
+        }
+    }
+
+    if (doPrint)
+    {
+        file.close();
+    }
+
+    // w tym przypadku wliczamy też czas dorzucenia do pliku i zliczenia
+    // trzebaby chyba ujednolicic wypisywanie wynikow wszędzie (określić jak definiujemy czas )
+    double endTime = omp_get_wtime();
+    std::cout << "Czas obliczania: " << endTime - startTime << " sekund" << std::endl;
+    std::cout << "dlugosc listy: " << totalPrimeCount << std::endl;
+
+    delete[] basePrimes;
+    delete[] primes;
+    delete[] segment;
+
+    return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
