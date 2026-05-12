5c952a8fd2993d8d172855a44f13c00f40d775a5
Kamil <kamil.suwiczak@wp.pl>
2026-05-12 12:51:32 +0200

    update collect_data

--- tests/collect_data.py
+++ tests/collect_data.py
@@ -7,7 +7,7 @@ dirs = os.listdir('../')
 filtered_dirs = [d for d in dirs if os.path.isdir(os.path.join('../', d)) and d.startswith('Projekt')]
 
 for i, dir in enumerate(filtered_dirs):
-    polecenie = [f"../{dir}/a.out"]
+    polecenie = [f"../{dir}/a.out", "-n", "25"]
     try:
         result = subprocess.run(polecenie, capture_output=True, text=True, check=True)
         print(f"Wyjście programu: {dir}:\n", result.stdout)
