bb3a39fd276d2e4a5b2a13460e521f3aef434373
mit3r <franek.kaczmarek45@gmail.com>
2026-05-14 12:06:38 +0200

    dzielenie sekwencyjne [k1] - clean up

--- Projekt1_Dzielenie_sekwencyjne/Projekt1_1_Dzielenie_sekwencyjne.cpp
+++ Projekt1_Dzielenie_sekwencyjne/Projekt1_1_Dzielenie_sekwencyjne.cpp
@@ -7,77 +7,76 @@
 #include <cstring>
 #include <omp.h>
 #include <algorithm>
+#include <time.h>
 
-int main(int argc, char **argv)
-{
+bool utils_doPrint = false;
+
+void utils_get_args(int argc, char** argv, int& m, int& n) {
+	for(int i = 0; i < argc; i++) {
+		if(!strcmp(argv[i], "-m") && i + 1 < argc)
+			m = std::atoi(argv[i + 1]);
+		if(!strcmp(argv[i], "-n") && i + 1 < argc)
+			n = std::atoi(argv[i + 1]);
+		if(!strcmp(argv[i], "-o")) {
+			utils_doPrint = true;
+		}
+	}
+}
+
+void utils_save_primes(bool* result, int m, int n) {
+	std::fstream file("primes.txt", std::ios::out);
+	for(int i = m; i <= n; i++) {
+		if(result[i - m]) {
+			file << i << std::endl;
+		}
+	}
+	file.close();
+}
+
+int main(int argc, char** argv) {
+
 	int m = 2, n = pow(10, 8);
 
-	for (int i = 0; i < argc; i++)
-	{
-		if (!strcmp(argv[i], "-m") && i + 1 < argc)
-			m = std::atoi(argv[i + 1]);
-		if (!strcmp(argv[i], "-n") && i + 1 < argc)
-			n = std::atoi(argv[i + 1]);
-	}
+	utils_get_args(argc, argv, m, n);
 
-	// Właściwy algorytm
+	bool* basePrimes = new bool[(int)(sqrt(n) + 1)];
+	std::memset(basePrimes, true, (sqrt(n) + 1) * sizeof(bool));
+	basePrimes[0] = basePrimes[1] = false;
 
-	bool *primeArray = new bool[(int)(sqrt(n) + 1)];
-	std::memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
-
-	bool *result = new bool[n - m + 1];
+	bool* result = new bool[n - m + 1];
 	std::memset(result, true, (n - m + 1) * sizeof(bool));
 
-	double startTime = omp_get_wtime();
-	for (int i = 2; i * i <= n; i++)
-	{
-		for (int j = 2; j * j <= i; j++)
-		{
-			if (primeArray[j] == true && i % j == 0)
-			{
-				primeArray[i] = false;
+	double startWallTime = clock();
+	double startProcTime = omp_get_wtime();
+
+	for(int i = 2; i * i <= n; i++) {
+		for(int j = 2; j * j <= i; j++) {
+			if(basePrimes[j] == true && i % j == 0) {
+				basePrimes[i] = false;
 				break;
 			}
 		}
 	}
 
-	for (int i = m; i <= n; i++)
-	{
-		for (int j = 2; j * j <= i; j++)
-		{
-			if (primeArray[j] == true && i % j == 0)
-			{
+	for(int i = m; i <= n; i++) {
+		for(int j = 2; j * j <= i; j++) {
+			if(basePrimes[j] == true && i % j == 0) {
 				result[i - m] = false;
 				break;
 			}
 		}
 	}
 
-	double endTime = omp_get_wtime();
-	std::cout << "Czas obliczania sekwencyjnych liczb pierwszych w przedziale [m, n]: " << endTime - startTime << " sekund" << std::endl;
+	double endWallTime = clock();
+	double endProcTime = omp_get_wtime();
 
-	// Wypisywanie wyników do pliku, jeśli podano flagę -o
-	bool doPrint = false;
-	for (int i = 0; i < argc; i++)
-		doPrint = !strcmp(argv[i], "-o");
+	std::cout << "Wall_clock_time: " << (endWallTime - startWallTime) / CLOCKS_PER_SEC << std::endl;
+	std::cout << "Processor_time: " << (endProcTime - startProcTime) << std::endl;
+	std::cout << "Primes_found: " << std::count(result, result + (n - m + 1), true) << std::endl;
 
-	if (doPrint)
-	{
-		std::fstream file("primes_1.txt", std::ios::out);
-
-		for (int i = m; i <= n; i++)
-		{
-			if (result[i - m])
-			{
-
-				file << i << std::endl;
-			}
-		}
-		std::cout << "dlugsc listy: " << std::count(result, result + (n - m + 1), true) << std::endl;
-		file.close();
-	}
+	utils_save_primes(result, m, n);
 
 	delete[] result;
-	delete[] primeArray;
+	delete[] basePrimes;
 	return 0;
 }
