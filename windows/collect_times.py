4dade450c19d43c83dc1cb9ef482caeb74cddebf
mit3r <franek.kaczmarek45@gmail.com>
2026-05-14 14:50:41 +0200

    testowanie programów celem zebrania informacji o czasach

--- windows/collect_times.py
+++ windows/collect_times.py
@@ -1,15 +1,33 @@
 
 import os
-import sys
 import subprocess
-
 
 codes = filter(lambda x: x.endswith(".exe"), os.listdir("./x64/Release/"))
 
+mini = 2
+half = 10e8 // 2
+maksi = 10e8
+
+variants = [
+	(mini, maksi), # pełny zakres
+	(half, maksi), # górny zakres
+	(mini, half), # dolny zakres
+	]
 
 for code in codes:
 	print(f"Uruchamiam {code}...")
 
 	path = os.path.join("./x64/Release/", code)
-	subprocess.run([path, "-n 2", "-m 1000000"], check=True) 
-	
+
+	for n, m in variants:
+
+		print(f"{code}: {n} - {m}")
+		name = f"./outputs/output_{code}_{n}_{m}.txt"
+
+		if os.path.exists(name):
+			print(f"Plik {name} już istnieje, pomijam...")
+			continue
+
+		output = open(name, "w")
+		subprocess.run([path, f"-n {n}", f"-m {m}"], check=True, stdout=output, stderr=output)
+		output.close()
